4daa8d307a9a929204abf7812c06470c89efa8e4
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-06-29 23:11:24 -0700

    made navbar, blog post input box and put them in thier component folders. also imported bootstrap aand used it for the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 
+function WelcomeMessage(){
+  return <h3>Welcome to Red Sauce</h3>
+}
+
 function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<h3>Welcome to Red Sauce</h3>
+        <WelcomeMessage />
         <p>Your personal Blog Post Application</p>
 
 			</header>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './Component/Navbar.jsx';
+import Blog from "./Component/Blog.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar />
     <App />
+    <Blog />
   </React.StrictMode>
 );
 

c72f6dbdb85cac641b4d8813a9d4e5c25bae79a7
Ali Rizvi <alirizvi@Starboys-Mac.local>
2022-07-02 11:13:23 -0700

    Created Lorem Ipsum text for home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,12 @@ function Home(props) {
         <div className='App'>
             <header className='App-header'>
                 <h3>Welcome to Red Sauce</h3>
-                <p>Your personal Blog Post Application</p>
+                <p>Your personal Blog Post Application<br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
+                    Sapiente iure dolorem blanditiis modi voluptatem eum ut deserunt fugit doloribus. <br />
+                    Quas vel ex ipsum facere sed quidem mollitia. Eos, quaerat doloremque.
+                </p>
+
             </header>
         </div>
     );

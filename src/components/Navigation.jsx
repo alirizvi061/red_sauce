80768a264e74169402b841a3cc788e63f0aeb2c7
Ali Rizvi <alirizvi@Starboys-Mac.local>
2022-07-02 12:30:40 -0700

    Created / from home page path

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,7 +4,7 @@ import { useHistory, Link } from "react-router-dom";
 function Navigation() {
     return (
         <div>
-            <a href="/home">Home</a>
+            <a href="/">Home</a>
             <a href="/about">about</a>
             <a href="/profile">profile</a>
         </div>

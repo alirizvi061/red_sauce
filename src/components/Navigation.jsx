9bbbe86b244595a157390ca4dd638053bf63e837
Ali Rizvi <alirizvi@Starboys-Mac.local>
2022-07-02 15:37:48 -0700

    trying to setup desing for navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 function Navigation() {
     return (
         <div>
-            <a href="/">Home</a>
-            <a href="/about">about</a>
-            <a href="/profile">profile</a>
-        </div>
+            <div id="navbar">
+                <a href="/">Home</a>
+                <a href="/about">about</a>
+                <a href="/profile">profile</a>
+            </div>
+        </div >
     )
 }
 
